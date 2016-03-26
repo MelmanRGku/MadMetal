2c1fdaab6f83238f7efedb04b156601657c9f879
Antonius-Hubertus <tom_diepstraten@hotmail.com>
2016-03-25 18:44:13 -0600

    Reverting to the old shader where the light source is embedded in the camera

--- MadMetal/Renderer/FragmentShader.glsl
+++ MadMetal/Renderer/FragmentShader.glsl
@@ -23,6 +23,13 @@ uniform vec3 ambient = vec3(0.1, 0.1, 0.1);
 uniform sampler2D texObject;
 uniform bool texValid;
 
+float specularSimple(vec3 L,vec3 N,vec3 H){
+   if(dot(N,L)>0){
+      return pow(clamp(dot(H,N),0.0,1.0),64.0);
+   }
+   return 0.0;
+}
+
 void main(void)
 {
     // Normalize the incoming N, L and V vectors
@@ -45,6 +52,18 @@ void main(void)
 	else
 		diffuse *= vec3(1, 1, 1);
 
+	vec3 specular = pow(max(dot(R, V), 0.0), specular_power) * specular_albedo;
+
+	vec3 halfVector = normalize( L + V );
+
+    float iambi = 0.1;
+    float idiff = clamp(dot(L,N),0.0,1.0);
+    float ispec = specularSimple(L, N, halfVector);
+    float intensity = iambi + idiff + ispec;
+
+    // quantize intensity for cel shading
+    float shadeIntensity = ceil(intensity * 5)/ 5;
+
 
 //	vec3 h = normalize(V + normalize(L - vec3(fs_in.position_attr) ) );
 
@@ -54,20 +73,21 @@ void main(void)
 //	else if (specular.x + specular.y + specular.z > 1.0) specular = vec3(0.25, 0.25, 0.25);
 //	else specular = vec3(0, 0, 0);
 
-	vec3 specular = pow(max(dot(N,L), 0.0), specular_power) * specular_albedo;
+//	vec3 specular = pow(max(dot(N,L), 0.0), specular_power) * specular_albedo;
 
 
     // Write final colour to the framebuffer
 	if (texValid)
 	{
 		float alpha = texture(texObject, fs_in.uv).w;
-		gl_FragColor = vec4(diffuse + specular, alpha);
+		//gl_FragColor = vec4(diffuse + specular, alpha);
+		gl_FragColor = texture(texObject, fs_in.uv);
 
 	} else {
-		gl_FragColor = vec4(diffuse + specular, fs_in.C.w);
+		//gl_FragColor = vec4(diffuse + specular, fs_in.C.w);
 
 	}
-
-	gl_FragColor = round(gl_FragColor * 8) / 8;
+		gl_FragColor = vec4(ambient + diffuse * shadeIntensity + specular * shadeIntensity, fs_in.C.w);
+	//gl_FragColor = round(gl_FragColor * 8) / 8;
 
 }
