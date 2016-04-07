d41f72e44570c0ed61fcc470a5eab622ab3113c6
DannyPhantom <igori1995@gmail.com>
2016-04-07 16:32:46 -0600

    Moved death pits up. Lap system works.

--- MadMetal/Objects/CollisionVolume.cpp
+++ MadMetal/Objects/CollisionVolume.cpp
@@ -6,6 +6,7 @@
 CollisionVolume::CollisionVolume(long id, Audioable *aable, Physicable *pable, Animatable *anable, Renderable3D *rable) : Object3D(id, aable, pable, anable, rable, NULL)
 {
 	m_respawnIndex = 0;
+	m_isStartCollisionVolume = false;
 }
 
 
@@ -37,16 +38,16 @@ PxTransform CollisionVolume::getRespawnLocation()
 	return transform;
 }
 
-void CollisionVolume::addAdjacentVolume(CollisionVolume * toAdd)
+void CollisionVolume::addNextVolume(CollisionVolume * toAdd)
 {
-	m_adjacentVolumes.push_back(toAdd);
+	m_nextVolumes.push_back(toAdd);
 }
 
-bool CollisionVolume::isAdjacent(CollisionVolume * toCheck)
+bool CollisionVolume::isPrevVolumeOf(CollisionVolume * toCheck)
 {
-	for (int i = 0; i < m_adjacentVolumes.size(); i++)
+	for (int i = 0; i < m_nextVolumes.size(); i++)
 	{
-		if (m_adjacentVolumes[i] == toCheck)
+		if (m_nextVolumes[i] == toCheck)
 		{
 			return true;
 		}
@@ -54,6 +55,15 @@ bool CollisionVolume::isAdjacent(CollisionVolume * toCheck)
 	return false;
 	
 }
+
+void CollisionVolume::setIsStartCollisionVolume(bool isStartCollisionVolume) {
+	m_isStartCollisionVolume = isStartCollisionVolume;
+}
+
+bool CollisionVolume::getIsStartCollisionVolume() {
+	return m_isStartCollisionVolume;
+}
+
 
 bool CollisionVolume::draw(Renderer *renderer, Renderer::ShaderType type, int passNumber) {
 #ifdef _RENDER_COLLISION_VOLUME
