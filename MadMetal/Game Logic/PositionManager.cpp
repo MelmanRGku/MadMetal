f0acd2b9d96ea3f4f1a911f9e2fbecc32ee91153
MelmanRGku <melmanrgku@gmail.com>
2016-04-12 21:20:47 -0600

    Fixed main positioning system. Edge cases still to come.

--- MadMetal/Game Logic/PositionManager.cpp
+++ MadMetal/Game Logic/PositionManager.cpp
@@ -55,26 +55,30 @@ void PositionManager::updatePlayerPositions()
 				}
 				else if (m_players.at(i)->getCar()->getCurrentCollisionVolume()->getId() == m_players.at(j)->getCar()->getCurrentCollisionVolume()->getId())
 				{
+					if (m_players.at(i)->getCar()->getLastMainPathCollisionVolume() == NULL || m_players.at(j)->getCar()->getLastMainPathCollisionVolume() == NULL)
+					{
+						continue;
+					}
 					glm::vec3 player1Position = m_players.at(i)->getCar()->getGlobalPose();
 					player1Position.y = 0;
 
 					glm::vec3 player2Position = m_players.at(j)->getCar()->getGlobalPose();
 					player2Position.y = 0;
 
-					glm::vec3 player1Goal = m_players.at(i)->getCar()->getCurrentCollisionVolume()->getGlobalPose();
+					glm::vec3 player1Goal = m_players.at(i)->getCar()->getLastMainPathCollisionVolume()->getGlobalPose();
 					player1Goal.y = 0;
 
-					glm::vec3 player2Goal = m_players.at(j)->getCar()->getCurrentCollisionVolume()->getGlobalPose();
+					glm::vec3 player2Goal = m_players.at(j)->getCar()->getLastMainPathCollisionVolume()->getGlobalPose();
 					player2Goal.y = 0;
 
 					float distanceToGoalPlayer1 = glm::distance2(player1Position, player1Goal);
 					float distanceToGoalPlayer2 = glm::distance2(player2Position, player2Goal);
 
-					if (distanceToGoalPlayer1 > distanceToGoalPlayer2 && m_players.at(j)->getCar()->getPositionInRace() > m_players.at(i)->getCar()->getPositionInRace())
+					if (distanceToGoalPlayer1 < distanceToGoalPlayer2 && m_players.at(j)->getCar()->getPositionInRace() > m_players.at(i)->getCar()->getPositionInRace())
 					{
 						swap(*m_players.at(i), *m_players.at(j));
 					}
-					else if (distanceToGoalPlayer1 < distanceToGoalPlayer2 && m_players.at(j)->getCar()->getPositionInRace() < m_players.at(i)->getCar()->getPositionInRace())
+					else if (distanceToGoalPlayer1 > distanceToGoalPlayer2 && m_players.at(j)->getCar()->getPositionInRace() < m_players.at(i)->getCar()->getPositionInRace())
 					{
 						swap(*m_players.at(i), *m_players.at(j));
 					}
